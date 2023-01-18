e128d5b8e4b909d65a8f85a989b38ae67623fbfb
VladimirRanski <ranski.vladimir@gmail.com>
2023-01-18 23:48:34 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        let result = Math.round((this.max + this.min) / 2);
+        return result
     }
 
     lower() {
-
+        this.max = this.guess()
     }
 
     greater() {
-
+        this.min = this.guess()
     }
 }
 
